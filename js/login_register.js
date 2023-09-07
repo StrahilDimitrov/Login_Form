9c34eca1ab8fc95525ffbb377c5eaec32dc399dc
Strahil Hristov <strahil1998@abv.bg>
2023-09-07 10:23:43 +0300

    first login form

---
+++ js/login_register.js
@@ -0,0 +1,15 @@
+var x = document.getElementById("login");
+var y = document.getElementById("register");
+var k = document.getElementById("btn");
+
+function register(){
+    x.style.left = "-400px";
+    y.style.left = "50px";
+    k.style.left = "110px";
+}
+
+function login(){
+    x.style.left = "50px";
+    y.style.left = "450px";
+    k.style.left = "0";
+}
